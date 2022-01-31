bfffde05aa032787938832a9bbd68b119e2acd39
hmazelier <hadrien.mazelier@supinfo.com>
2022-01-31 15:48:08 +0100

    get rid of appsync and unecessary bits

--- Package.swift
+++ Package.swift
@@ -11,31 +11,12 @@ let package = Package(
             name: "Amplify",
             targets: ["Amplify"]),
         
-        .library(name: "AWSPluginsCore",
-                 targets: ["AWSPluginsCore"]),
-        
-        .library(name: "AWSAPIPlugin",
-                 targets: ["AWSAPIPlugin"]),
-        
         .library(name: "AWSCognitoAuthPlugin",
                  targets: ["AWSCognitoAuthPlugin"]),
-        
-        .library(name: "AWSDataStorePlugin",
-                 targets: ["AWSDataStorePlugin"]),
-        
-        .library(name: "AWSLocationGeoPlugin",
-                 targets: ["AWSLocationGeoPlugin"]),
-        
-        .library(name: "AWSPinpointAnalyticsPlugin",
-                 targets: ["AWSPinpointAnalyticsPlugin"]),
-        
-        .library(name: "AWSS3StoragePlugin",
-                 targets: ["AWSS3StoragePlugin"]),
         
     ],
     dependencies: [
         .package(name: "AWSiOSSDKV2", url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", .upToNextMinor(from: "2.27.0")),
-        .package(name: "AppSyncRealTimeClient", url: "https://github.com/aws-amplify/aws-appsync-realtime-client-ios.git", from: "1.4.3"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", .exact("0.12.2"))
     ],
     targets: [
@@ -57,20 +38,6 @@ let package = Package(
             ]
         ),
         .target(
-            name: "AWSAPIPlugin",
-            dependencies: [
-                .target(name: "Amplify"),
-                .target(name: "AWSPluginsCore"),
-                .product(name: "AWSCore", package: "AWSiOSSDKV2"),
-                .product(name: "AppSyncRealTimeClient", package: "AppSyncRealTimeClient")
-            ],
-            path: "AmplifyPlugins/API/AWSAPICategoryPlugin",
-            exclude: [
-                "Info.plist",
-                "AWSAPIPlugin.md"
-            ]
-        ),
-        .target(
             name: "AWSCognitoAuthPlugin",
             dependencies: [
                 .target(name: "Amplify"),
@@ -85,55 +52,5 @@ let package = Package(
                 "Resources/Info.plist"
             ]
         ),
-        .target(
-            name: "AWSDataStorePlugin",
-            dependencies: [
-                .target(name: "Amplify"),
-                .target(name: "AWSPluginsCore"),
-                .product(name: "SQLite", package: "SQLite.swift")],
-            path: "AmplifyPlugins/DataStore/AWSDataStoreCategoryPlugin",
-            exclude: [
-                "Info.plist"
-            ]
-        ),
-        .target(
-            name: "AWSLocationGeoPlugin",
-            dependencies: [
-                .target(name: "Amplify"),
-                .target(name: "AWSPluginsCore"),
-                .product(name: "AWSCore", package: "AWSiOSSDKV2"),
-                .product(name: "AWSLocationXCF", package: "AWSiOSSDKV2")
-            ],
-            path: "AmplifyPlugins/Geo/AWSLocationGeoPlugin",
-            exclude: [
-                "Resources/Info.plist"
-            ]
-        ),
-        .target(
-            name: "AWSPinpointAnalyticsPlugin",
-            dependencies: [
-                .target(name: "Amplify"),
-                .target(name: "AWSPluginsCore"),
-                .product(name: "AWSCore", package: "AWSiOSSDKV2"),
-                .product(name: "AWSPinpoint", package: "AWSiOSSDKV2")
-            ],
-            path: "AmplifyPlugins/Analytics/AWSPinpointAnalyticsPlugin",
-            exclude: [
-                "Resources/Info.plist"
-            ]
-        ),
-        .target(
-            name: "AWSS3StoragePlugin",
-            dependencies: [
-                .target(name: "Amplify"),
-                .target(name: "AWSPluginsCore"),
-                .product(name: "AWSCore", package: "AWSiOSSDKV2"),
-                .product(name: "AWSS3", package: "AWSiOSSDKV2")
-            ],
-            path: "AmplifyPlugins/Storage/AWSS3StoragePlugin",
-            exclude: [
-                "Resources/Info.plist"
-            ]
-        )
     ]
 )
